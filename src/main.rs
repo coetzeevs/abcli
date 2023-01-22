5facbe301a6d82018d716bc389d77f498f5d3c0d
Coetzee van Staden <coetzeev@saltpay.co>
2023-01-22 13:32:00 +0000

    refactor: main to use new project struct

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,64 @@
 #![allow(unused)]
 
 mod adapters;
+mod cli;
 
 mod prelude {
-    pub use crate::adapters::api::client::API;
-    pub use crate::adapters::slack::client::Slack;
-
+    // add adapters
+    pub use crate::adapters::api::client as api_client;
+    pub use crate::adapters::slack::client as slack_client;
+    pub use crate::adapters::slack::stringer as slack_stringer;
+    // add cli
+    pub use crate::cli::commands::slack::Commands;
 }
 use crate::prelude::*;
 
-use anyhow::{Context, Result};
-use clap::Parser;
 
-/// Search for a pattern in a file and display the lines that contain it.
+use ::clap::{Parser, Subcommand};
+use crate::cli::commands::slack::Status;
+
+
 #[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
+#[command(author, version)]
+#[command(about = "A Basic CLI a.k.a. abcli - a simple CLI to do mundane things", long_about = "abcli is a super fancy CLI (kidding)
+You can use abcli to do various things, but at the moment it does nothing...")]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
-fn main() -> Result<()> {
-    let _d = API {};
-    let _f = Slack {};
+fn main() {
+    let parsed_cli = Cli::parse();
 
-    let args = Cli::parse();
-
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("Could not read file `{}`", &args.path.display()))?;
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    match &parsed_cli.command {
+        Some(Commands::Status(name)) => {
+            match name.status {
+                Some(ref _name) => {
+                    let reverse = slack_stringer::reverse(_name);
+                    println!("{}", reverse);
+                }
+                None => {
+                    println!("Please provide a string to reverse");
+                }
+            }
         }
+        None => {}
     }
-    Ok(())
 }
+
+// saving this for later reference
+// let _d = API {};
+// let _f = Slack {};
+
+// use anyhow::{Context, Result};
+
+
+// let content = std::fs::read_to_string(&args.path)
+//     .with_context(|| format!("Could not read file `{}`", &args.path.display()))?;
+
+// for line in content.lines() {
+//     if line.contains(&args.pattern) {
+//         println!("{}", line);
+//     }
+// }
+// Ok(())
