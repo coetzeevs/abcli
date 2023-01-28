db23d8862f12ce555517d25acd51424c7ae8bae8
Coetzee van Staden <coetzeev@saltpay.co>
2023-01-28 08:23:20 +0000

    add: helper function to construct HeaderMap from HashMap

---
+++ src/adapters/api/helpers.rs
@@ -0,0 +1,11 @@
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+
+
+pub fn construct_headers(headers: std::collections::HashMap<HeaderName, &'static String>) -> HeaderMap {
+    let mut h_map = HeaderMap::new();
+
+    for (k, v) in headers {;
+        h_map.entry(k).or_insert_with(|| { HeaderValue::from_static(v.as_str()) });
+    }
+    h_map
+}
