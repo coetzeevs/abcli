021694e0580adb9617c735e834683cdddf5f5d8d
Coetzee van Staden <coetzeev@saltpay.co>
2023-01-28 14:19:28 +0000

    refactor: spotify client implementation to use new header method

--- src/adapters/spotify/client.rs
+++ src/adapters/spotify/client.rs
@@ -1,26 +1,47 @@
 // use core::result::Result::Ok;
 use reqwest::StatusCode;
-use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::header::{HeaderValue, ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 
+use crate::adapters::api::client::client;
+use crate::adapters::api::models::{Header, Headers};
 use crate::adapters::spotify::models::APIResponse;
-// use crate::adapters::spotify::helpers::print_tracks;
 
-#[tokio::main]
+
+// TODO: finish
+fn set_auth() {}
+
+fn set_headers() -> Headers {
+    let fake_auth = "";
+    let headers = Headers {
+        headers: vec![
+            Header {
+                header_name: ACCEPT,
+                header_value: HeaderValue::from_static("application/json")
+            },
+            Header {
+                header_name: AUTHORIZATION,
+                header_value: HeaderValue::from_str(format!("Bearer {}", fake_auth)
+                    .as_str())
+                    .unwrap()
+            },
+            Header {
+                header_name: CONTENT_TYPE,
+                header_value: HeaderValue::from_static("application/json")
+            },
+        ]
+    };
+    headers
+}
+
 pub async fn query(query: &String) -> Result<(), Box<dyn std::error::Error>> {
-    let auth_token = &String::from("");
+
     let url = format!(
         "https://api.spotify.com/v1/search?q={query}&type=track,artist",
         query = query
     );
-    let client = reqwest::Client::new();
-    let response = client
-        .get(url)
-        .header(AUTHORIZATION, format!("Bearer {}", auth_token))
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
-        .await
-        .unwrap();
+    
+    let response = client(url, set_headers());
+
     match response.status() {
         StatusCode::OK => {
             match response.json::<APIResponse>().await {
@@ -36,7 +57,6 @@ pub async fn query(query: &String) -> Result<(), Box<dyn std::error::Error>> {
         }
     };
     // TODO: cleanup response
-    //       implement base client
     //       add more commands and parameters
     Ok(())
 }
