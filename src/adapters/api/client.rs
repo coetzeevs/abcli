96dd62b0d3a8654a541467507fed969ec594c94e
Coetzee van Staden <coetzeev@saltpay.co>
2023-01-28 08:22:55 +0000

    add: generic api client

--- src/adapters/api/client.rs
+++ src/adapters/api/client.rs
@@ -1,1 +1,16 @@
-pub struct API {}
+use reqwest::header::HeaderName;
+use crate::adapters::api::helpers::construct_headers;
+
+#[tokio::main]
+pub async fn client(url: String, headers: std::collections::HashMap<HeaderName, &'static String>) -> reqwest::Response {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .headers(construct_headers(headers))
+        .send()
+        .await
+        .unwrap();
+
+    return response;
+}
