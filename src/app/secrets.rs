546d669e73e18e59c70f5897f39f6f194cacdf0b
Coetzee van Staden <coetzeev@saltpay.co>
2023-01-29 21:14:29 +0000

    [REFACTOR] - secure-store: making it easy to use (#6)

--- src/app/secrets.rs
+++ src/app/secrets.rs
@@ -1,5 +1,5 @@
 use securestore::{KeySource, SecretsManager};
-use std::{path::Path, collections::HashMap};
+use std::path::Path;
 use once_cell::sync::Lazy;
 
 
@@ -15,11 +15,17 @@ pub static SECRETS: Lazy<SecretsManager> = Lazy::new(|| {
         .expect("Failed to load SecureStore vault!")
 });
 
-pub fn get_secret(secret_map: &mut HashMap<String, String>)
-    -> Result<HashMap<&String, String>, securestore::Error> {
-        let mut ret = HashMap::new();
-        for (k, _) in secret_map.iter_mut() {
-            ret.insert(k, SECRETS.get(k.as_str())?);
-        }
-    Ok(ret)
+pub fn get_secret(key: &str) -> std::string::String {
+    SECRETS.get(key).expect("Failed to retrieve secret")
 }
+
+// TODO: look into writing own type with implemented functions to retrieve map & value
+// Leaving this here for later
+// pub fn get_secret_map(secret_map: &mut HashMap<String, String>)
+//     -> Result<HashMap<&String, String>, securestore::Error> {
+//         let mut ret = HashMap::new();
+//         for (k, _) in secret_map.iter_mut() {
+//             ret.insert(k, SECRETS.get(k.as_str())?);
+//         }
+//     Ok(ret)
+// }
