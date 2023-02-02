81fb7fe457189f02c66e997b3fe34f881d4b9c23
Coetzee van Staden <coetzeev@saltpay.co>
2023-02-02 19:41:21 +0000

    update: "abstracting" out client calls

--- src/adapters/api/client.rs
+++ src/adapters/api/client.rs
@@ -1,5 +1,7 @@
 use crate::adapters::api::helpers::construct_headers;
 use crate::adapters::api::models::Headers;
+use crate::adapters::notion::models::page::Request;
+
 
 pub async fn client(url: String, headers: Headers) -> reqwest::Response {
     let client = reqwest::Client::new();
@@ -13,3 +15,33 @@ pub async fn client(url: String, headers: Headers) -> reqwest::Response {
 
     response
 }
+
+pub async fn get(url: String, headers: Headers) -> reqwest::Response {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .headers(construct_headers(headers))
+        .send()
+        .await
+        .unwrap();
+
+    response
+}
+
+pub async fn post(url: String, headers: Headers, body: &Request) -> reqwest::Response {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .post(url)
+        .headers(construct_headers(headers))
+        .header("Notion-Version", "2022-06-28")
+        .json(body)
+        .send()
+        .await
+        .unwrap();
+
+    response
+}
+
+// TODO: this needs a lot of cleanup. This is gross..
