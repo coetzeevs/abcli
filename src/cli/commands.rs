ab2b0a2f370830f444c4a3d7be7fccd254b90b1b
Coetzee van Staden <coetzeev@saltpay.co>
2023-02-02 19:40:46 +0000

    add: notion cli commands

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,3 +1,4 @@
 pub mod base;
+pub mod notion;
 pub mod slack;
 pub mod spotify;
--- src/cli/commands/base.rs
+++ src/cli/commands/base.rs
@@ -2,6 +2,8 @@ use ::clap::Subcommand;
 
 use crate::cli::commands::slack::SlackArgs;
 use crate::cli::commands::spotify::SpotifyArgs;
+// TODO: look into using super notation i.s.o. using crate::
+use super::notion::NotionArgs;
 
 // first tier subcommands - e.g. acbli slack or abcli evernote
 #[derive(Subcommand)]
@@ -10,4 +12,6 @@ pub enum Commands {
     Slack(SlackArgs),
     /// Command to interface with various methods from the Spotify API
     Spotify(SpotifyArgs),
+    /// Command to interface with various method from the Notion API
+    Notion(NotionArgs)
 }
